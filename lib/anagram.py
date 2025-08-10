bb438947ffd901de427378bae8aaadf3e1f2aa59
+ <+githinjiadreen27@gmail.com>
2025-08-10 12:38:12 +0300

    Implement Anagram class with match method

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+#creates a list of letters in alphabetic order.
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+
+#loop through all words in the given list
+        matches = []
+        for candidate in word_list:
+            if candidate.lower() != self.word and sorted(candidate.lower()) == sorted_word:
+                matches.append(candidate)
+
+        return matches
+
+anagram = Anagram("listen")
+#list 
+print(anagram.match(["enlist", "google", "inlets", "banana"]))
+        
+
+        
+
+
